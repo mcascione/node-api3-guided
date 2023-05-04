3b3854ee5f9c98646859e8c8268940699350f8e9
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-04 14:15:39 -0700

    added in error handling middleware for hubs-router

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -1,120 +1,85 @@
-const express = require('express');
+const express = require("express");
 
-const Hubs = require('./hubs-model.js');
-const Messages = require('../messages/messages-model.js');
+const Hubs = require("./hubs-model.js");
+const Messages = require("../messages/messages-model.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res, next) => {
   Hubs.find(req.query)
-    .then(hubs => {
+    .then((hubs) => {
       res.status(200).json(hubs);
     })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the hubs',
-      });
-    });
+    .catch(next);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res, next) => {
   Hubs.findById(req.params.id)
-    .then(hub => {
+    .then((hub) => {
       if (hub) {
         res.status(200).json(hub);
       } else {
-        res.status(404).json({ message: 'Hub not found' });
+        res.status(404).json({ message: "Hub not found" });
       }
     })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error retrieving the hub',
-      });
-    });
+    .catch(next);
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res, next) => {
   Hubs.add(req.body)
-    .then(hub => {
+    .then((hub) => {
       res.status(201).json(hub);
     })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error adding the hub',
-      });
-    });
+    .catch(next);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res, next) => {
   Hubs.remove(req.params.id)
-    .then(count => {
+    .then((count) => {
       if (count > 0) {
-        res.status(200).json({ message: 'The hub has been nuked' });
+        res.status(200).json({ message: "The hub has been nuked" });
       } else {
-        res.status(404).json({ message: 'The hub could not be found' });
+        res.status(404).json({ message: "The hub could not be found" });
       }
     })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error removing the hub',
-      });
-    });
+    .catch(next);
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res, next) => {
   Hubs.update(req.params.id, req.body)
-    .then(hub => {
+    .then((hub) => {
       if (hub) {
         res.status(200).json(hub);
       } else {
-        res.status(404).json({ message: 'The hub could not be found' });
+        res.status(404).json({ message: "The hub could not be found" });
       }
     })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error updating the hub',
-      });
-    });
+    .catch(next);
 });
 
-router.get('/:id/messages', (req, res) => {
+router.get("/:id/messages", (req, res, next) => {
   Hubs.findHubMessages(req.params.id)
-    .then(messages => {
+    .then((messages) => {
       res.status(200).json(messages);
     })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error getting the messages for the hub',
-      });
-    });
+    .catch(next);
 });
 
-router.post('/:id/messages', (req, res) => {
+router.post("/:id/messages", (req, res, next) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   Messages.add(messageInfo)
-    .then(message => {
+    .then((message) => {
       res.status(210).json(message);
     })
-    .catch(error => {
-      // log error to server
-      console.log(error);
-      res.status(500).json({
-        message: 'Error adding message to the hub',
-      });
-    });
+    .catch(next);
+});
+
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message,
+    customMessage: "Something bad happened inside the hubs router",
+  });
 });
 
 module.exports = router;
